0934ec8ab4f2ff6c92124640c996dd8ab0400113
Leor Yamay <Leor.yamay@gmail.com>
2024-05-03 12:15:14 +0300

    test4

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ const corsOptions = {
         'http://127.0.0.1:5173',
         'http://127.0.0.1:3000',
         'http://localhost:5173',
-        'http://localhost:3000'
+        'http://localhost:3000',
+        'https://coding-academy-nodeserver-miss-bug-proj.onrender.com:3000',
+        'https://coding-academy-nodeserver-miss-bug-proj.onrender.com:5173'
     ],
     credentials: true
 }
